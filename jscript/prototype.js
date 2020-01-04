ee7963a641d0494725a0ee3d18b3e0f2fd6a2c98
pverkind <peter.verkinderen@gmail.com>
2020-01-04 18:20:19 +0100

    Correct behaviour of backspace, delete, left and right arrow keys

--- jscript/prototype.js
+++ jscript/prototype.js
@@ -2,6 +2,12 @@ function sl(id) {
     return document.getElementById(id);
 }
 
+var arLet = "\u061C";
+var vowSep = "\u180E";
+//var arLet = "-";
+//var vowSep = "_";
+
+
 
 function wr(item) {
     var input = sl('area');
@@ -10,16 +16,19 @@ function wr(item) {
         to fix the display order for every character from right to left*/
         /*item = item + "\u061C";*/
         item = item.split("");
-        item = item.join("\u061C")+"\u061C";
+        /*item = item.join("\u061C") + "\u061C";*/
+        item = item.join(arLet) + arLet;
     } else {
-        item = item.replace("\u061C", "");
+        /*item = item.replace("\u061C", "");*/
+        item = item.replace(arLet, "");
     }
     if (sl("regexCheck").checked) {
         /* add a MONGOLIAN VOWEL SEPARATOR (\u180E) before every character
         to separate the Arabic characters */
         /*item = "\u180E" + item;*/
         item = item.split("");
-        item = "\u180E"+item.join("\u180E");        
+        /*item = "\u180E"+item.join("\u180E");*/
+        item = vowSep + item.join(vowSep);
     } else {
         item = item.replace("\u25cc", ""); /*remove the dotted circles from vowels*/
     }
@@ -28,7 +37,7 @@ function wr(item) {
         var srt = input.selectionStart;
         var len = input.selectionEnd;
         if (srt < len) srt++;
-        input.value = input.value.substr(0, srt) + item + input.value.substr(len);
+        input.value = input.value.substring(0, srt) + item + input.value.substring(len);
         input.setSelectionRange(srt + item.length, srt + item.length);
     } else {
         var range = document.selection.createRange();
@@ -40,7 +49,7 @@ function wr(item) {
 
 
 
-function setSelectionRange(input, selectionStart, selectionEnd) {
+/*function setSelectionRange(input, selectionStart, selectionEnd) {
     if (input.setSelectionRange) {
         input.focus();
         input.setSelectionRange(selectionStart, selectionEnd);
@@ -51,50 +60,129 @@ function setSelectionRange(input, selectionStart, selectionEnd) {
         range.moveStart("character", selectionStart);
         range.select();
     }
+}*/
+
+// Move the cursor position to just after the previous real character
+// (before any ALM or character separator)
+function toPrevChar(pos, txt) {
+  //console.log(pos);
+  if (pos > 0) {
+    var precededByNonChar = [arLet, vowSep].includes(txt.substring(pos-1, pos));
+    while (precededByNonChar) {
+      precededByNonChar = [arLet, vowSep].includes(txt.substring(pos-1, pos));
+      if (precededByNonChar) pos--;
+    }
+  }
+  return pos;
 }
 
+// Move the cursor position to just before the next real character
+// (after any nonChar (i.e., ALM or character separator))
+function toNextChar(pos, txt) {
+  var followedByNonChar = true;
+  while (followedByNonChar) {
+    followedByNonChar = [arLet, vowSep].includes(txt.substring(pos, pos+1));
+    if (followedByNonChar) pos++;
+  }
+  return pos;
+}
 
+function getSelection(target) {
+  target.focus();
+  var srt = target.selectionStart;
+  var end = target.selectionEnd;
+  return [srt, end];
+}
+
+function arrowR() {
+  var target = sl("area");
+  [srt, end] = getSelection(target);
+  //console.log([srt, end]);
+  if (srt < end) {  // if something is selected, move cursor to start of selection
+    srt = toPrevChar(srt, target.value);
+  } else {  // if nothing is selected, move cursor to position before previous character
+    srt = toPrevChar(end, target.value);
+    srt--;
+  }
+  target.setSelectionRange(srt, srt);
+  target.focus();
+}
+
+function arrowL() {
+  var target = sl("area");
+  [srt, end] = getSelection(target);
+  if (srt < end) {  // if something is selected, move cursor to end of selection
+    srt = toNextChar(end, target.value);
+  } else {  // if nothing is selected, move cursor to position after next character
+    srt = toNextChar(end, target.value);
+    srt++;
+  }
+  target.setSelectionRange(srt, srt);
+  target.focus();
+}
+
+function remove(rmType) {
+  // rmType is a string, either "del" or "backspace".
+  var target = sl("area");
+  [srt, end] = getSelection(target);
+  if (srt < end) {  // if something is selected, remove selection
+    srt = toPrevChar(srt, target.value);
+    end = toPrevChar(end, target.value);
+  } else {  // if nothing is selected
+    srt = toPrevChar(srt, target.value);
+    end = toNextChar(end, target.value);
+    if (rmType === "del") {  // remove following character
+      end = Math.min(target.value.length, (end+1));
+    } else if (rmType === "backspace") { // remove preceding character
+      srt = Math.max(0, (srt-1));
+    }
+  }
+  target.value = target.value.substring(0, srt) + target.value.substring(end);
+  target.setSelectionRange(srt, srt);
+  target.focus();
+  return true;
+}
+
+function del() {
+  remove("del");
+  /*var target = sl("area");
+  [srt, end] = getSelection(target);
+  if (srt < end) {  // if something is selected, remove selection
+    var srt = toPrevChar(srt, target.value);
+    var end = toPrevChar(end, target.value);
+  } else {  // if nothing is selected, remove following character
+    var srt = toPrevChar(srt, target.value);
+    var end = toNextChar(end, target.value);
+    end = Math.min(target.value.length, (end+1));
+  }
+  target.value = target.value.substring(0, srt) + target.value.substring(end);
+  target.setSelectionRange(srt, srt);
+  target.focus();
+  return true;*/
+}
+
+function backspace() {
+  remove("backspace");
+  /*var target = sl("area");
+  [srt, end] = getSelection(target);
+  //console.log("selection start: " + srt + ", end: " + end);
+  if (srt < end) {  // if something is selected, remove selection
+    var srt = toPrevChar(srt, target.value);
+    var end = toPrevChar(end, target.value);
+  } else {  // if nothing is selected, remove preceding character
+    var srt = toPrevChar(srt, target.value);
+    var end = toNextChar(end, target.value);
+    srt = Math.max(0, (srt-1));
+  }
+  target.value = target.value.substring(0, srt) + target.value.substring(end);
+  target.setSelectionRange(srt, srt);
+  target.focus();
+  return true;*/
+}
 
 function op(el) {
     var input = sl("area").value;
-    var encode = encodeURI(input);
     if (el == "back") {
-        var target = sl("area");
-        target.focus();
-        if (target.setSelectionRange) {
-            var srt = target.selectionStart;
-            var len = target.selectionEnd;
-            if (srt < len) srt++;
-            if (sl("regexCheck").checked && sl("regexCheck0").checked) {
-                target.value = target.value.substr(0, srt - 3) + target.value.substr(len);
-            } else if (sl("regexCheck").checked || sl("regexCheck0").checked) {
-                target.value = target.value.substr(0, srt - 2) + target.value.substr(len);
-            } else {
-                target.value = target.value.substr(0, srt - 1) + target.value.substr(len);
-            }
-            target.setSelectionRange(srt - 1, srt - 1);
-            target.focus();
-        } else
-        if (target.createTextRange) {
-            self.VKI_range = document.selection.createRange();
-            try {
-                self.VKI_range.select();
-            }
-            catch (e) {
-            }
-            self.VKI_range = document.selection.createRange();
-            if (! self.VKI_range.text.length)
-            if (sl("regexCheck").checked && sl("regexCheck0").checked) {
-                self.VKI_range.moveStart('character', -3);
-            } else if (sl("regexCheck").checked || sl("regexCheck0").checked) {
-                self.VKI_range.moveStart('character', -2);
-            } else {
-                self.VKI_range.moveStart('character', -1);
-            }
-            self.VKI_range.text = "";
-            target.focus();
-        } else target.value = target.value.substr(0, target.value.length - 1);
-        target.focus();
-        return true;
+      backspace();
     }
 }
